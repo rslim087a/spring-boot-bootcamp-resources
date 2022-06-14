26a336cc0e4fc7e523f83288d91f6c3311a6a6be
Rayan Slim <rayanslim@Rayans-MacBook-Air.local>
2022-06-14 19:43:54 -0400

    starter update

--- 2. model-view-controller/workbook 2.3/solution/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 2. model-view-controller/workbook 2.3/solution/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -3,14 +3,14 @@ package com.ltp.workbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class WorkbookController {
 
     @GetMapping(value="/")
-    public String getSign() {
+    public String getSign(Model model) {
+        model.addAttribute("speed", 70);
         return "sign";
     }
     
