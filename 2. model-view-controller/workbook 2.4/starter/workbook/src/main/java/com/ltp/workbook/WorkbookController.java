bf46d2976b3a6aeff2051736224843a3a4cbab1b
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-06-17 17:58:12 -0400

    Update WorkbookController.java

--- 2. model-view-controller/workbook 2.4/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ 2. model-view-controller/workbook 2.4/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -3,8 +3,6 @@ package com.ltp.workbook;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class WorkbookController {
