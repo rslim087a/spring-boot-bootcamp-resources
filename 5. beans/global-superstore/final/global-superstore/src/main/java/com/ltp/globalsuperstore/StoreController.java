8a47f39d39e7f01327bfa7443e5bdb0cf93c49e7
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-06-22 04:14:58 -0400

    Update StoreController.java

--- 5. beans/global-superstore/final/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ 5. beans/global-superstore/final/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -16,7 +16,7 @@ public class StoreController {
 
     StoreService storeService;
 
-    @Autowired //not needed (see workbook article)
+    @Autowired //not actually needed (see workbook article)
     public StoreController(StoreService storeService) {
         this.storeService = storeService;
     }
