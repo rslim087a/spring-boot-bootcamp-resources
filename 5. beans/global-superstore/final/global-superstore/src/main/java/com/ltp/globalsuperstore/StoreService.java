3e895e7e63100a5e7cccc407295a1312b7a8ebae
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-06-22 03:25:27 -0400

    Update StoreService.java

--- 5. beans/global-superstore/final/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreService.java
+++ 5. beans/global-superstore/final/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreService.java
@@ -13,7 +13,7 @@ public class StoreService {
 
     StoreRepository storeRepository;
     
-    @Autowired
+    @Autowired //not needed (see workbook article)
     public StoreService(StoreRepository storeRepository) {
         this.storeRepository = storeRepository;
     }
