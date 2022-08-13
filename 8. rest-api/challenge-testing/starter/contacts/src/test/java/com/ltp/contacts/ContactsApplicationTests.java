e544bc8673e7e601c16208de748f5ea996cbae67
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-08-13 02:21:00 -0400

    Update ContactsApplicationTests.java

--- 8. rest-api/challenge-testing/starter/contacts/src/test/java/com/ltp/contacts/ContactsApplicationTests.java
+++ 8. rest-api/challenge-testing/starter/contacts/src/test/java/com/ltp/contacts/ContactsApplicationTests.java
@@ -2,6 +2,8 @@ package com.ltp.contacts;
 
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.ltp.contacts.pojo.Contact;
