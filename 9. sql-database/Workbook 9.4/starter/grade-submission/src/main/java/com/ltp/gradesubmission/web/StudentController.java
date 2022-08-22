7f0e88cd0745fca165186fb325dd25f0afcf0003
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-08-22 18:04:22 -0400

    Update StudentController.java

--- 9. sql-database/Workbook 9.4/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
+++ 9. sql-database/Workbook 9.4/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/StudentController.java
@@ -23,7 +23,6 @@ import lombok.AllArgsConstructor;
 @RequestMapping("/student")
 public class StudentController {
 
-    @Autowired
     StudentService studentService;
     
     @GetMapping("/{id}")
