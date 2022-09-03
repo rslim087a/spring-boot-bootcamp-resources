4ffcc4710da006f3c6fd1d1ff82b630e629f6503
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-09-03 17:13:56 -0400

    Update GradeSubmissionApplication.java

--- 10. spring-security/workbook 10.1/final/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
+++ 10. spring-security/workbook 10.1/final/grade-submission/src/main/java/com/ltp/gradesubmission/GradeSubmissionApplication.java
@@ -42,19 +42,6 @@ public class GradeSubmissionApplication implements CommandLineRunner {
 		for (int i = 0; i < students.length; i++) {
 			studentRepository.save(students[i]);
 		}
-
-		Course[] courses = new Course[] {
-            new Course("Charms", "CH104", "In this class, you will learn spells concerned with giving an object new and unexpected properties."),
-            new Course("Defence Against the Dark Arts", "DADA", "In this class, you will learn defensive techniques against the dark arts."),
-            new Course("Herbology", "HB311", "In this class, you will learn the study of magical plants and how to take care of, utilise and combat them."),
-            new Course("History of Magic", "HIS393", "In this class, you will learn about significant events in wizarding history."),
-            new Course("Potions", "POT102", "In this class, you will learn correct mixing and stirring of ingredients to create mixtures with magical effects."),
-            new Course("Transfiguration", "TR442", "In this class, you will learn the art of changing the form or appearance of an object.")
-        };
-
-		for (int i = 0; i < courses.length; i++) {
-			courseRepository.save(courses[i]);
-		}
 		
 	}
 
