c39417e4174ebaa92491c007d39ee28c089426fe
rslim087a <42172016+rslim087a@users.noreply.github.com>
2022-09-09 04:39:05 -0400

    Update SecurityConstants.java

--- 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConstants.java
+++ 10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConstants.java
@@ -1,7 +1,7 @@
 package com.ltp.gradesubmission.security;
 
 public class SecurityConstants {
-    public static final String SECRET_LEY = "fL7;hF8@gH5yM8!fU4~e"; //Your secret should always be strong (uppercase, lowercase, numbers, symbols) so that nobody can potentially decode the signature.
+    public static final String SECRET_KEY = "bQeThWmZq4t7w!z$C&F)J@NcRfUjXn2r5u8x/A?D*G-KaPdSgVkYp3s6v9y$B&E)"; //Your secret should always be strong (uppercase, lowercase, numbers, symbols) so that nobody can potentially decode the signature.
     public static final int TOKEN_EXPIRATION = 7200; // 7200 seconds = 2 hours.
     public static final String BEARER = "Bearer "; // Authorization : "Bearer " + Token 
     public static final String AUTHORIZATION = "Authorization"; // "Authorization" : Bearer Token
