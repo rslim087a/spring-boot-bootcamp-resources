8ca2b9c943047cb36dd383c9ef0a768db53a1a8e
rslim087a <42172016+rslim087a@users.noreply.github.com>
2025-01-15 13:36:26 -0500

    Update Constants.java

--- 03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
+++ 03. field-validation/global-superstore/starter/global-superstore/src/main/java/com/ltp/globalsuperstore/Constants.java
@@ -4,5 +4,4 @@ public class Constants {
     public static final int NOT_FOUND = -10000;
     public static final String SUCCESS_STATUS = "success";
     public static final String FAILED_STATUS = "failed";
-    public static final String[] CATEGORIES = new String[] {"Furniture", "Office Supplies", "Technology"};
 }
